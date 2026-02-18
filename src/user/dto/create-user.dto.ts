4d5ee6c07b4c0fd44ad0320609f3aa9c8f9b3844
PrakritPrad <poom66310@gmail.com>
2026-02-18 23:48:57 +0700

    user/dto: add some dto {create update}

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { UserRole, UserStatus } from '../entities/user.entity';
+import { IsString, IsEmail, IsEnum, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  fullName!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  phone!: string;
+
+  @IsEnum(UserRole)
+  role!: UserRole;
+
+  @IsString()
+  organizationId!: string;
+
+  @IsString()
+  department!: string;
+
+  @IsString()
+  position!: string;
+
+  @IsOptional()
+  @IsEnum(UserStatus)
+  status?: UserStatus;
+}
