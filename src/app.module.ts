96f0ed5d3ebfcbb0d38e6a02c125e74eda3abe5b
PrakritPrad <poom66310@gmail.com>
2026-02-20 15:09:56 +0700

    src/: re structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { SkibidiModule } from './skibidi/skibidi.module';
 import { OrganizationModule } from './organization/organization.module';
+import { OrganizationModule } from './organization/organization.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, OrganizationModule],
+  imports: [UserModule, OrganizationModule, SkibidiModule],
   controllers: [],
   providers: [],
 })
