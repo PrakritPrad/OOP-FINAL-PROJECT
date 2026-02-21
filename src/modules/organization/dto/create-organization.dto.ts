86e351afd10c059c2b9cbd9b115d27a91451001b
PrakritPrad <poom66310@gmail.com>
2026-02-21 13:45:32 +0700

    src/orgamization : add entities and dto

--- src/modules/organization/dto/create-organization.dto.ts
+++ src/modules/organization/dto/create-organization.dto.ts
@@ -1,1 +1,54 @@
-export class CreateOrganizationDto {}
+import { OrganizationType, OrganizationStatus } from '../entities/organization.entity';
+import { IsString, IsEmail, IsEnum, IsOptional, IsNumber, IsUrl } from 'class-validator';
+
+export class CreateOrganizationDto {
+  @IsString()
+  name!: string;
+
+  @IsString()
+  registrationNumber!: string;
+
+  @IsString()
+  description!: string;
+
+  @IsString()
+  industry!: string;
+
+  @IsEnum(OrganizationType)
+  type!: OrganizationType;
+
+  @IsOptional()
+  @IsEnum(OrganizationStatus)
+  status?: OrganizationStatus;
+
+  @IsUrl()
+  website!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  phone!: string;
+
+  @IsString()
+  address!: string;
+
+  @IsString()
+  city!: string;
+
+  @IsString()
+  country!: string;
+
+  @IsString()
+  foundedDate!: string;
+
+  @IsNumber()
+  numberOfEmployees!: number;
+
+  @IsOptional()
+  @IsString()
+  logo?: string;
+
+  @IsString()
+  taxId!: string;
+}
--- src/modules/organization/entities/organization.entity.ts
+++ src/modules/organization/entities/organization.entity.ts
@@ -1,1 +1,37 @@
-export class Organization {}
+export enum OrganizationStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  SUSPENDED = 'SUSPENDED',
+  ARCHIVED = 'ARCHIVED',
+}
+
+export enum OrganizationType {
+  CORPORATE = 'CORPORATE',
+  NGO = 'NGO',
+  STARTUP = 'STARTUP',
+  GOVERNMENT = 'GOVERNMENT',
+  EDUCATION = 'EDUCATION',
+  HEALTHCARE = 'HEALTHCARE',
+}
+
+export class Organization {
+  id!: string;
+  name!: string;
+  registrationNumber!: string;
+  description!: string;
+  industry!: string;
+  type!: OrganizationType;
+  status!: OrganizationStatus;
+  website!: string;
+  email!: string;
+  phone!: string;
+  address!: string;
+  city!: string;
+  country!: string;
+  foundedDate!: string;
+  numberOfEmployees!: number;
+  logo?: string;
+  taxId!: string;
+  createdAt!: string;
+  updatedAt!: string;
+}
